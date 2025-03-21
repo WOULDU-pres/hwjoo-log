9852e1db3392bac46af1fb3615b27b50e1dc784b
WOULDU-pres <jhw664089@gmail.com>
2025-03-21 17:18:44 +0900

    first commit

---
+++ site.config.js
@@ -0,0 +1,75 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "주현우",
+    image: "/blog_main.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "backend developer",
+    bio: "이벤트에 미친 사람",
+    email: "jhw102258@gmail.com",
+    linkedin: "morethanmin",
+    github: "https://github.com/WOULDU-pres",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `vent`,
+      href: "https://github.com/WOULDU-pres/vent?tab=readme-ov-file",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "hwjoo",
+    description: "welcome to hwjoo!",
+    scheme: "light", // 'light' | 'dark' | 'system'
+  },
+
+  // CONFIG configration (required)
+  link: "https://hwjoo.vercel.app",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 600, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
